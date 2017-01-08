8ac15190545ad31c791bbb3367d5454300fb3f12
Nedelin Yordanov <nedeliny@gmail.com>
2017-01-09 00:05:03 +0200

    Add tile shader class

--- src/main.js
+++ src/main.js
@@ -1,10 +1,134 @@
 import * as PIXI from 'pixi.js';
 import MiniSignal from 'mini-signals';
 
+const rectShaderFrag = `
+varying vec2 vTextureCoord;
+varying vec4 vFrame;
+varying float vTextureId;
+uniform vec4 shadowColor;
+uniform sampler2D uSamplers[%count%];
+uniform vec2 uSamplerSize[%count%];
 
-class TileMapRenderer extends PIXI.ObjectRenderer{
+void main(void){
+  vec2 textureCoord = clamp(vTextureCoord, vFrame.xy, vFrame.zw);
+  float textureId = floor(vTextureId + 0.5);
+
+  vec4 color;
+  %forloop%
+  gl_FragColor = color;
+}`;
+
+const rectShaderVert = `
+attribute vec2 aVertexPosition;
+attribute vec2 aTextureCoord;
+attribute vec4 aFrame;
+attribute vec2 aAnim;
+attribute float aTextureId;
+
+uniform mat3 projectionMatrix;
+uniform vec2 animationFrame;
+
+varying vec2 vTextureCoord;
+varying float vTextureId;
+varying vec4 vFrame;
+
+void main(void){
+  gl_Position = vec4((projectionMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
+  vec2 anim = aAnim * animationFrame;
+  vTextureCoord = aTextureCoord + anim;
+  vFrame = aFrame + vec4(anim, anim);
+  vTextureId = aTextureId;
+}`;
+
+const generateFragmentSrc = (maxTextures, fragmentSrc) => {
+  return fragmentSrc.replace(/%count%/gi, maxTextures + "")
+    .replace(/%forloop%/gi, this.generateSampleSrc(maxTextures));
+}
+
+const generateSampleSrc = (maxTextures) => {
+  let src = '';
+
+  src += '\n';
+  src += '\n';
+
+  src += 'if(vTextureId <= -1.0) {';
+  src += '\n\tcolor = shadowColor;';
+  src += '\n}';
+
+  for(let i = 0; i < maxTextures; i++){
+    src += '\nelse ';
+
+    if(i < maxTextures-1){
+        src += 'if(textureId == ' + i + '.0)';
+    }
+
+    src += '\n{';
+    src += '\n\tcolor = texture2D(uSamplers['+i+'], textureCoord * uSamplerSize['+i+']);';
+    src += '\n}';
+  }
+
+  src += '\n';
+  src += '\n';
+
+  return src;
+}
+
+const fillSamplers = (shader, maxTextures)=>{
+  const sampleValues = [];
+  for (let i = 0; i < maxTextures; i++){
+    sampleValues[i] = i;
+  }
+  shader.bind();
+  shader.uniforms.uSamplers = sampleValues;
+
+  const samplerSize = [];
+  for (i = 0; i < maxTextures; i++) {
+      samplerSize.push(1.0 / 2048);
+      samplerSize.push(1.0 / 2048);
+  }
+  shader.uniforms.uSamplerSize = samplerSize;
+}
+
+class TileShader extends PIXI.Shader{
+  maxTextures = 0;
+  indexBuffer = null;
+
+  constructor(gl, maxTextures, vert, frag, ){
+    super(gl, vert, frag);
+    this.maxTextures = maxTextures;
+    fillSamplers(this, this.maxTextures);
+  }
+}
+
+class RectTileShader extends TileShader{
+  vertSize = 11;
+  vertPerQuad = 4;
+  stride = this.vertSize * 4;
+  
+  constructor(gl, maxTextures) {
+    super(gl,
+      maxTextures,
+      rectShaderVert,
+      generateFragmentSrc(maxTextures, rectShaderFrag)
+    );
+    fillSamplers(this, this.maxTextures);
+  }
+
+  createVao(renderer, vb) {
+    const gl = renderer.gl;
+    return renderer.createVao()
+      .addIndex(this.indexBuffer)
+      .addAttribute(vb, this.attributes.aVertexPosition, gl.FLOAT, false, this.stride, 0)
+      .addAttribute(vb, this.attributes.aTextureCoord, gl.FLOAT, false, this.stride, 2 * 4)
+      .addAttribute(vb, this.attributes.aFrame, gl.FLOAT, false, this.stride, 4 * 4)
+      .addAttribute(vb, this.attributes.aAnim, gl.FLOAT, false, this.stride, 8 * 4)
+      .addAttribute(vb, this.attributes.aTextureId, gl.FLOAT, false, this.stride, 10 * 4);
+  }
+}
+
+class TileRenderer extends PIXI.ObjectRenderer{
   constructor(renderer) {
-    super(renderer)
+    super(renderer);
   }
 }
 
@@ -18,7 +142,7 @@ export default class Main extends PIXI.Container{
       console.log("DOM loaded...");
       this.init();
       this.renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
-      this.renderer.backgroundColor = 0x333333;
+      this.renderer.backgroundColor = 0x222222;
       document.body.appendChild(this.renderer.view);
       this.renderer.render(this);
     });
